bc83f8c60c51755337b0908e5bdb1b4ae0ac79f5
Qusid <siddhan10@gmail.com>
2021-09-21 23:53:46 -0400

    1. Home Screen 2. Nav bar  2.1 Drunk test screen  2.2 Profile Screen  2.3 Settings Screen
    Nav bar implemented

--- app/src/main/java/com/example/drunkapp/MainActivity.kt
+++ app/src/main/java/com/example/drunkapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.drunkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    override fun onStart() {
+        super.onStart()
+        setupBottomNavigationMenu()
+    }
+    private fun setupBottomNavigationMenu() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragments)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
 }
